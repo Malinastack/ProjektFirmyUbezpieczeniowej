8ada51e61dcf60e333c58675554d941b3e8bdf65
Szymon Malinowski <mszymon962@gmail.com>
2022-02-08 22:54:55 +0100

    database in progress

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
-from .models import Person
+from .models import Client, Car
 # Register your models here.
 
 
-class Test(admin.ModelAdmin):
+class CreatingClient(admin.ModelAdmin):
     fields = ['first_name', 'last_name']
 
 
-admin.site.register(Person, Test)
+admin.site.register(Client, CreatingClient)
+
+
+class CreatingCar(admin.ModelAdmin):
+    fields = ['mark', 'production_year', 'plate_numbers']
+
+
+admin.site.register(Car, CreatingCar)
--- polls/models.py
+++ polls/models.py
@@ -4,9 +4,37 @@ from django.db import models
 from django.db import models
 
 
-class Person(models.Model):
+class Client(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
+
+class Insurance(models.Model):
+    NNW = 'NNW'
+    Autocasco = 'Autocasco'
+    Assistance = 'Assistance'
+    OC = 'OC'
+    policy_type_choices = (
+        (NNW, 'NNW'),
+        (Autocasco, 'Autocasco'),
+        (Assistance, 'Assistance'),
+        (OC, 'OC'),
+
+    )
+    policy_number = models.CharField(max_length=30)
+    policy_type = models.CharField(max_length=10, choices=policy_type_choices, default=OC)
+    policy_end_date = models.DateField()
+
+
+class Car(models.Model):
+    mark = models.CharField(max_length=20)
+    production_year = models.DateField()
+    plate_numbers = models.CharField(max_length=20)
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE, null=True, blank=True)
+    insurance_policy = models.ForeignKey(Insurance, on_delete=models.CASCADE, null=True, blank=True)
+
+
+
+
 
 
 
