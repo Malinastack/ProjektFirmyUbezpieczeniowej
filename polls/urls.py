4cf420f57850e4ceffc7605d96d35b26929d13c8
Szymon Malinowski <mszymon962@gmail.com>
2022-02-10 22:54:48 +0100

    added client_list view

---
+++ polls/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:client_id>/', views.client, name='client')
+]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from .models import Client
+
+
+def index(request):
+   clients_list = Client.objects.order_by('-first_name')[:5]
+   context = {
+       'clients_list': clients_list,
+   }
+   return render(request, 'polls/index.html', context)
+
+
+def client(request, client_id):
+    response = "Dane osoby %s."
+    return HttpResponse(response % client_id)
