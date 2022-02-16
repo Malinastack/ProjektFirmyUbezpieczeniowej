761ac934ba5b16ba0a0d5db4246813bd9b2df60e
Szymon Malinowski <mszymon962@gmail.com>
2022-02-16 15:45:54 +0100

    some login view changes

--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,10 @@ from django import forms
 from django.urls import reverse_lazy
 from .models import Client
 from django.views.generic import DetailView, ListView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ClientListView(ListView):
+class ClientListView(LoginRequiredMixin, ListView):
     model = Client
 
 
